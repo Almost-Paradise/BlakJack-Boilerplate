d750c22970840670990ba205270e9add5c66a3f9
BlakJack <arshmalikrs@yahoo.co.in>
2014-12-07 03:30:25 +0530

    Fixes

--- commands.js
+++ commands.js
@@ -467,17 +467,13 @@ var commands = exports.commands = {
 		if (targetUser) targetUser.updateIdentity();
 		if (room.chatRoomData) Rooms.global.writeChatRoomData();
 	},
-	reload: function (target, room, user) {
+reload: function (target, room, user) {
         if (!this.can('reload')) return;
 
         try {
             this.sendReply('Reloading CommandParser...');
-            CommandParser.uncacheTree(path.join(__dirname, './', 'command-parser.js'));
-            CommandParser = require(path.join(__dirname, './', 'command-parser.js'));
-
-            this.sendReply('Reloading Bot...');
-            CommandParser.uncacheTree(path.join(__dirname, './', 'bot.js'));
-            Bot = require(path.join(__dirname, './', 'bot.js'));
+            CommandParser.uncacheTree(path.join(__dirname, './', './command-parser.js'));
+            CommandParser = require(path.join(__dirname, './', './command-parser.js'));
 
             this.sendReply('Reloading Tournaments...');
             var runningTournaments = Tournaments.tournaments;
@@ -486,22 +482,31 @@ var commands = exports.commands = {
             Tournaments.tournaments = runningTournaments;
             
             this.sendReply('Reloading Core...');
-            CommandParser.uncacheTree(path.join(__dirname, './', './core.js'));
-            Core = require(path.join(__dirname, './', './core.js')).core;
+            CommandParser.uncacheTree(path.join(__dirname, './', './source/core.js'));
+            Core = require(path.join(__dirname, './', './source/core.js')).core;
+			
+			this.sendReply('Reloading Hangman...');
+            CommandParser.uncacheTree(path.join(__dirname, './', './source/hangman.js'));
+            hangman = require(path.join(__dirname, './', './source/hangman.js')).hangman;
 
-            this.sendReply('Reloading Components...');
-            CommandParser.uncacheTree(path.join(__dirname, './', './components.js'));
-            Components = require(path.join(__dirname, './', './components.js'));
-
-            this.sendReply('Reloading SysopAccess...');
-            CommandParser.uncacheTree(path.join(__dirname, './', './core.js'));
-            SysopAccess = require(path.join(__dirname, './', './core.js'));
+            this.sendReply('Reloading custom-commands.js...');
+	        CommandParser.uncacheTree(path.join(__dirname, './', './source/custom-commands.js'));
+	        customcommands = require(path.join(__dirname, './', './source/custom-commands.js'));
+	        CommandParser.uncacheTree(path.join(__dirname, './', './source/trainer-cards.js'));
+	        trainercards = require(path.join(__dirname, './', './source/trainer-cards.js'));
+			
+            this.sendReply('Reloading SysopAccess...');	
+            CommandParser.uncacheTree(path.join(__dirname, './', './source/core.js'));
+            SysopAccess = require(path.join(__dirname, './', './source/core.js'));
 
             return this.sendReply('|raw|<font color="green">All files have been reloaded.</font>');
         } catch (e) {
             return this.sendReply('|raw|<font color="red">Something failed while trying to reload files:</font> \n' + e.stack);
         }
     },
+
+
+
 	roomauth: function (target, room, user, connection) {
 		var targetRoom = room;
 		if (target) targetRoom = Rooms.search(target);
